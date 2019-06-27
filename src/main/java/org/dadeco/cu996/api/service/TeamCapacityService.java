b86de1b626acb97b1ae5278f56e49ed839cbfd4b
QIN Jamie (CI/DAD-AP) <qji2sgh@bosch.com>
2019-06-27 20:35:13 +0800

    Update Personal Capacity Overview

--- src/main/java/org/dadeco/cu996/api/service/TeamCapacityService.java
+++ src/main/java/org/dadeco/cu996/api/service/TeamCapacityService.java
@@ -2,8 +2,6 @@ package org.dadeco.cu996.api.service;
 
 import java.util.List;
 
-import org.dadeco.cu996.api.model.User;
-
 public interface TeamCapacityService {
-	public List<List<Object>> getTeamCapacityByMonth(User user, int startYearMonth, int endYearMonth);
+	public List<List<Object>> getTeamCapacityByMonth(String startDate, String endDate);
 }
--- src/main/java/org/dadeco/cu996/api/service/impl/PersonalCapacityServiceImpl.java
+++ src/main/java/org/dadeco/cu996/api/service/impl/PersonalCapacityServiceImpl.java
@@ -2,7 +2,7 @@ package org.dadeco.cu996.api.service.impl;
 
 import java.util.List;
 
-import org.dadeco.cu996.api.model.User;
+import org.dadeco.cu996.api.model.RuntimeUserInfo;
 import org.dadeco.cu996.api.repository.ActivityRepository;
 import org.dadeco.cu996.api.service.PersonalCapacityService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +13,11 @@ public class PersonalCapacityServiceImpl implements PersonalCapacityService {
 	@Autowired
 	private ActivityRepository activityRepository = null;
 
-	public List<List<Object>> getPersonalCapacityByMonth(User user, int startYearMonth, int endYearMonth) {
+	public List<List<Object>> getPersonalCapacityByMonth(RuntimeUserInfo user, String startDate, String endDate) {
 		List<List<Object>> activities = null;
 
 		if (user != null) {
-			activities = activityRepository.findByUserIdAndStartAndEnd(user.getId(), startYearMonth, endYearMonth);
-		} else {
-			activities = activityRepository.findByStartAndEnd(startYearMonth, endYearMonth);
+			activities = activityRepository.findByUserIdAndStartAndEnd(user.getNtAccount(), startDate, endDate);
 		}
 
 		return activities;
